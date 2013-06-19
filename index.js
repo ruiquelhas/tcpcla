3b955aa7b9b2ea6b3997da3d4401891e457472af
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-19 17:43:20 +0100

    test a new design approach

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ var INVALID_INPUT =
 
 var isInteger = function (input) {
   return typeof input === 'number' && input % 1 === 0;
-}
+};
 
 var isIntegerBasedString = function (input) {
   return typeof input === 'string' && isInteger(parseInt(input, 10));
-}
+};
 
 var isSupported = function (input) {
   return isInteger(input) || isIntegerBasedString(input);
-}
+};
 
-var TCPCLA = function () { }
+var TCPCLA = {
+  createServer: net.createServer,
+  connect: net.connect
+};
 
 module.exports = TCPCLA;
--- test/all.js
+++ test/all.js
@@ -1,5 +1,20 @@
-var test = require('tap').test;
+var 
+  test = require('tap').test,
+  cla = require('../index');
 
-test('make sure the TCP connection exists with a valid CLA', function (t) {
-  t.end();
+test('make sure the node can listen to CL connections', function (t) {
+  t.plan(2);
+  var server = cla.createServer(), port = 4556, client;
+  server.listen(port, function () {
+    client = cla.connect(port);
+    client.on('end', function () {
+      t.ok(client, 'a client should be able to connect');
+    });
+    client.on('error', function (err) {
+      t.comment(err.message);
+    });
+    server.close(function () {
+      t.ok(server, 'the agent should be able to close gracefully');
+    });
+  });
 });
