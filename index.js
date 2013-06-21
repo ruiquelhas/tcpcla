b58c4d47f6864edc2a399635cc2862c8475646b6
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-21 14:27:40 +0100

    make cla API similar to the net API
    * add callback parameter to cla.createServer
    * add toolkit helper function to check if an argument is defined

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var net = require('net');
+var toolkit = require('./lib/toolkit');
 
-function createServer(header) {
+function createServer(header, callback) {
   return net.createServer().on('connection', function (socket) {
+    if (toolkit.isDefined(callback)) {
+      callback(socket);
+    }
     socket.write(header.valueOf());
   });
 }
