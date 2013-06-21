39e2cdeb3cdb69825a8873e5604c7775f746a80a
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-21 15:02:49 +0100

    refactor cla.createServer
    * add helper function to check for the existence of server options
    * change main function to acknowledge option arguments
    * add toolkit helpers to detect object and function types

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var net = require('net');
 var toolkit = require('./lib/toolkit');
 
-function createServer(header, callback) {
-  return net.createServer().on('connection', function (socket) {
-    if (toolkit.isDefined(callback)) {
-      callback(socket);
+function getServerInstance(options) {
+  if (!toolkit.isFunction(options) && toolkit.isObjectLiteral(options)) {
+    return net.createServer(options);
+  }
+  return net.createServer();
+}
+
+function createServer(header, arg1, arg2) {
+  return getServerInstance(arg1).on('connection', function (socket) {
+    if (toolkit.isDefined(arg2)) {
+      arg2(socket);
     }
     socket.write(header.valueOf());
   });
